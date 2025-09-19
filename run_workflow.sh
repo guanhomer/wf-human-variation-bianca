f4f326e139d807fdb71753962fc7e97235a5c32b
Ryan SY Kwan <guanhomer@gmail.com>
2025-09-19 13:10:58 +0200

    Create run_workflow.sh

---
+++ run_workflow.sh
@@ -0,0 +1,29 @@
+module load bioinfo-tools Nextflow snpEff_data/5.1
+
+export NXF_HOME=/castor/project/proj_nobackup/tools/nextflow/.nextflow
+export NXF_SINGULARITY_CACHEDIR=/proj/nobackup/sens2024549/human-variation-workflow/epi2me-labs/singularity/
+export NXF_OFFLINE='true'
+export NXF_OPTS='-Xms1g -Xmx4g'
+
+export WORKFLOW_DIR=/proj/nobackup/sens2024549/human-variation-workflow/epi2me-labs/wf-human-variation
+export CUSTOM_CONFIG=/proj/nobackup/sens2024549/human-variation-workflow/epi2me-labs/wf-human-variation/uppmax.config
+
+export BAM=/proj/sens2024549/nobackup/human-variation-workflow/20250919_BT-65_FF/bam/
+export REF=/proj/sens2024549/reference/GRCh38.p14.genome.fa
+# export BED=${DATA}demo.bed
+
+nextflow run $WORKFLOW_DIR \
+ --bam $BAM \
+ --ref $REF \
+ --sample_name 'BT-65_FF' \
+ --snp \
+ --sv \
+ --mod \
+ --phased \
+ --snpeff_data $SNPEFF_DATA \
+ -profile singularity \
+ --threads 1 \
+ --override_basecaller_cfg 'dna_r10.4.1_e8.2_400bps_sup@v5.2.0' \
+ -c $CUSTOM_CONFIG \
+ --project sens2024549 \
+ -resume
